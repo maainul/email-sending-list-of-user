b4b3cb70aa3c04497d521cd0a8c992f076e69882
maainul <mainul080@yahoo.com>
2021-04-07 02:01:12 +0600

    email sending done

--- src/main/java/com/sendEmail/emailsendinglistofuser/controller/EmployeeController.java
+++ src/main/java/com/sendEmail/emailsendinglistofuser/controller/EmployeeController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sendEmail.emailsendinglistofuser.model.Employee;
+import com.sendEmail.emailsendinglistofuser.repository.EmployeeRepository;
 import com.sendEmail.emailsendinglistofuser.service.EmployeeService;
 
 @Controller
@@ -26,7 +28,9 @@ public class EmployeeController {
 
 	@Autowired
 	private JavaMailSender mailSenderObj;
-	
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
 	@GetMapping("/")
 	public String welcomePage() {
@@ -52,44 +56,44 @@ public class EmployeeController {
 	}
 
 	// sendmail code
-		@GetMapping(value = "/sendMail/{employee_id}")
-		public ResponseEntity<Employee> employeeMail(@PathVariable("employee_id") Integer employee_id) {
+	@GetMapping(value = "/sendMail/{employee_id}")
+	public ResponseEntity<Employee> employeeMail(@PathVariable("employee_id") Integer employee_id) {
 
-			Employee employee = employeeService.findEmployeeById(employee_id);
+		Employee employee = employeeRepository.findById(employee_id).get();
 
-			sendmail(employee);
+		sendmail(employee);
 
-			return new ResponseEntity<Employee>(HttpStatus.OK);
-
-		}
-
-		private void sendmail(Employee employee) {
-
-			final String emailToRecipient = employee.getEmp_email();
-			final String emailSubject = "Succesfully Registration";
-
-			final String emailMessage1 = "<html> <body> <p>Dear Sir/Madam,</p><p>You have succesfully Registered with our Services"
-					+ "<br><br>"
-					+ "<table border='1' width='300px' style='text-align:center;font-size:20px;'><tr> <td colspan='2'>"
-					+ "</td></tr><tr><td>Name</td><td>" + employee.getEmp_name() + "</td></tr><tr><td>Address</td><td>"
-					+ employee.getEmp_address() + "</td></tr><tr><td>Email</td><td>" + employee.getEmp_email()
-					+ "</td></tr></table> </body></html>";
-
-			mailSenderObj.send(new MimeMessagePreparator() {
-
-				@Override
-				public void prepare(MimeMessage mimeMessage) throws Exception {
-
-					MimeMessageHelper mimeMsgHelperObj = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-
-					mimeMsgHelperObj.setTo(emailToRecipient);
-					mimeMsgHelperObj.setText(emailMessage1, true);
-
-					mimeMsgHelperObj.setSubject(emailSubject);
-
-				}
-			});
-
-		}
+		return new ResponseEntity<Employee>(HttpStatus.OK);
 
 	}
+
+	private void sendmail(Employee employee) {
+
+		final String emailToRecipient = employee.getEmp_email();
+		final String emailSubject = "Succesfully Registration";
+
+		final String emailMessage1 = "<html> <body> <p>Dear Sir/Madam,</p><p>You have succesfully Registered with our Services"
+				+ "<br><br>"
+				+ "<table border='1' width='300px' style='text-align:center;font-size:20px;'><tr> <td colspan='2'>"
+				+ "</td></tr><tr><td>Name</td><td>" + employee.getEmp_name() + "</td></tr><tr><td>Address</td><td>"
+				+ employee.getEmp_address() + "</td></tr><tr><td>Email</td><td>" + employee.getEmp_email()
+				+ "</td></tr></table> </body></html>";
+
+		mailSenderObj.send(new MimeMessagePreparator() {
+
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+
+				MimeMessageHelper mimeMsgHelperObj = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+				mimeMsgHelperObj.setTo(emailToRecipient);
+				mimeMsgHelperObj.setText(emailMessage1, true);
+
+				mimeMsgHelperObj.setSubject(emailSubject);
+
+			}
+		});
+
+	}
+
+}
